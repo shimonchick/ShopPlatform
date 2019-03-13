29879fe36eda5408e5c5c7cc4756d656b724d63e
DenisBarzanov <denis.barzanov2002@gmail.com>
2019-03-13 18:50:01 +0200

    Merge branch 'products' of https://github.com/shimonimium/ShopPlatform into github/products
    # Conflicts:
    #	package-lock.json

--- src/app/products/product-bottom-sheet/product-bottom-sheet.component.ts
+++ src/app/products/product-bottom-sheet/product-bottom-sheet.component.ts
@@ -1,19 +1,18 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_BOTTOM_SHEET_DATA} from '@angular/material';
+import {Component, Inject} from '@angular/core';
+import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from '@angular/material';
+import {Product} from '../../models/product';
 
 @Component({
     selector: 'app-product-bottom-sheet',
     templateUrl: './product-bottom-sheet.component.html',
     styleUrls: ['./product-bottom-sheet.component.scss']
 })
-export class ProductBottomSheetComponent implements OnInit {
+export class ProductBottomSheetComponent {
 
-    constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: any) {
-        console.log('%c DATAAA', 'color: brown');
-        console.log(data);
+    constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: Product, private bottomSheetRef: MatBottomSheetRef) {
     }
 
-    ngOnInit() {
+    dismiss() {
+        this.bottomSheetRef.dismiss();
     }
-
 }
