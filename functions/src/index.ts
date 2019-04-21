4b53b8f6a80f9a11ca51e090fe881afb4d4ab1dd
DenisBarzanov <denis.barzanov2002@gmail.com>
2019-04-21 14:18:26 +0300

    Deleted some accidentally committed files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,4 +3,3 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 export {indexProduct, unindexProduct, search} from './algolia';
 export {archiveChat} from './archive_chat';
-export {calculator} from './calculator';
