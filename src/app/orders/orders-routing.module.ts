6db647030265c96680bfaa087d3549dea22f9e63
shimonchick <shimonchick@gmail.com>
2019-04-27 01:55:11 +0300

    seller orders flex and guards

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {BuyerOrderListComponent} from './buyer-order-list/buyer-order-list.component';
 import {SellerOrderListComponent} from './seller-order-list/seller-order-list.component';
+import {AuthGuard} from '../core/auth.guard';
+import {SellerGuard} from '../core/seller.guard';
 
 const routes: Routes = [
-    {path: 'buyer', component: BuyerOrderListComponent},
-    {path: 'seller', component: SellerOrderListComponent},
+    {path: 'buyer', canActivate: [AuthGuard], component: BuyerOrderListComponent},
+    {path: 'seller', canActivate: [AuthGuard, SellerGuard], component: SellerOrderListComponent},
     // {path: ':id', component: OrderComponent}
 ];
 
