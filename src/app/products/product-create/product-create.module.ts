a3c018278c0e23e011abbf45fc7a4b60d359fb51
shimonchick <shimonchick@gmail.com>
2019-05-03 21:59:59 +0300

    Remove uploader and upload task components from product create module

--- src/app/products/product-create/product-create.module.ts
+++ src/app/products/product-create/product-create.module.ts
@@ -1,7 +1,5 @@
 import {NgModule} from '@angular/core';
 import {ProductCreateComponent} from './product-create.component';
-import {UploaderComponent} from './uploader/uploader.component';
-import {UploadTaskComponent} from './upload-task/upload-task.component';
 import {SharedModule} from '../../shared.module';
 import {ProductCreateRoutingModule} from './product-create-routing.module';
 import {ReactiveFormsModule} from '@angular/forms';
@@ -16,8 +14,6 @@ import {ProductSharedModule} from '../product-shared.module';
 @NgModule({
     declarations: [
         ProductCreateComponent,
-        UploaderComponent,
-        UploadTaskComponent,
         // ImageComponent,
         ImagePreviewComponent,
         CategoriesComponent,
